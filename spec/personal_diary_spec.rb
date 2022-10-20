873d71b894c209178aa5595ea21cea2d34c49ca6
Elliott Zaki <elliottzaki@Elliotts-MacBook-Air.local>
2022-10-20 16:32:27 +0100

    Passing test to ensure 5 words in a string are returned.

--- spec/personal_diary_spec.rb
+++ spec/personal_diary_spec.rb
@@ -11,7 +11,14 @@ describe '#make_snippet' do
     context 'given a four word string' do
         it 'should return the first four words' do
             result = 'one two three four'
-            expect(make_snippet('one two three four')).to eq('one two three four')
+            expect(make_snippet(result)).to eq('one two three four')
+        end
+    end
+
+    context 'given a five word string' do
+        it 'should return the first five words' do
+            result = 'one two three four five'
+            expect(make_snippet(result)).to eq('one two three four five')
         end
     end
 end
